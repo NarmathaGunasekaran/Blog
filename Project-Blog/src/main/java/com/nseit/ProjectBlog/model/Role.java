bd99672d98e78b926f71863b1d8a6baff799948f
NarmathaGunasekaran <narmathanandhini09@gmail.com>
2022-09-02 14:43:45 +0530

    commit

--- Project-Blog/src/main/java/com/nseit/ProjectBlog/model/Role.java
+++ Project-Blog/src/main/java/com/nseit/ProjectBlog/model/Role.java
@@ -2,14 +2,21 @@ package com.nseit.ProjectBlog.model;
 
 import lombok.Data;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
-//@Entity
+@Entity
 @Data
 public class Role {
+    public static final String ROLE_USER = "CUSTOMER";
+    public static final String ROLE_ADMIN = "ADMIN";
+
     @Id
-    @GeneratedValue
-    private Integer id;
-    private String role;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles", cascade = CascadeType.ALL)
+    private Set<Users> bookUsers;
+
 }
--- Project-Blog/src/main/java/com/nseit/ProjectBlog/model/Users.java
+++ Project-Blog/src/main/java/com/nseit/ProjectBlog/model/Users.java
@@ -1,9 +1,12 @@
 package com.nseit.ProjectBlog.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.hibernate.annotations.Cascade;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
@@ -12,9 +15,20 @@ public class Users {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
-    private String name;
-    private String email;
-    private Integer password;
+    private String userName;
+    private String password;
     @ManyToMany(mappedBy = "users", cascade = CascadeType.ALL)
     private List<Post> postModels;
+
+    @ManyToMany
+    @JsonIgnore
+    @Cascade({org.hibernate.annotations.CascadeType.ALL})
+    @JoinTable(joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private Set<Role> roles;
+
+    public Users(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
 }
